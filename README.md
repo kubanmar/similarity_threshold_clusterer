705e68827dec6e8e20a99ed6860a1513a5e50f2a
Martin Kuban <kuban@physik.hu-berlin.de>
2024-06-13 15:51:52 +0200

    change install file to pyproject.toml

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This Python package provides a clustering method for finding small compact clusters in a high dimensional descriptor space.
 
-The algorithm implemented in this package is introduced, explained, and used in Ref.[1].
+The algorithm implemented in this package is introduced, explained, and used in Ref. [1].
 
 [1] "Density-of-states similarity descriptor for unsupervised learning from materials data" by Martin Kuban, Santiago Rigamonti, Markus Scheidgen, and Claudia Draxl. https://arxiv.org/abs/2201.02187
--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="threshold_clusterer",
-    version="1.0",
-    author="Martin Kuban",
-    author_email="kuban@physik.hu-berlin.de",
-    description="A clustering method for small clusters in high dimensional descriptor spaces.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://git.physik.hu-berlin.de/kuban/threshold-clusterer",
-    python_requires='>=3.6',
-    install_requires = ['numpy'],
-    packages=['threshold_clusterer']
-)
+setuptools.setup()
